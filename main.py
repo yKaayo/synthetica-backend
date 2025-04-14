d9728bc82669a2972e12285d949e2bcdea1c34e5
yKaayo <caiokaea@gmail.com>
2025-04-13 21:11:43 -0300

    Get, post, patch and delete

--- main.py
+++ main.py
@@ -13,9 +13,12 @@ app.add_middleware(
 
 posts = [
     {
+        "id": 1,
         "title": "Pao",
         "description": "Pao gostosão",
-        "author": "Kaayo"
+        "author": "Kaayo",
+        "content": "ABC",
+        "category": 0
     }
 ]
 
@@ -23,13 +26,42 @@ class Post(BaseModel):
     title: str
     description: str
     author: str
+    content: str
+    category: int
 
 @app.post("/post")
 def create_post(post: Post):
-    posts.append(post)
+    new_id = posts[-1]["id"] + 1 if posts else 1
+    new_post = post.model_dump()
+    new_post["id"] = new_id
+    posts.append(new_post)
     print(post)
+    print(new_post)
+    print(posts)
     return {"Message": "Post adicionado com sucesso!", "post": post}
 
 @app.get("/posts")
 def get_all_posts():
     return posts
+
+@app.patch("/post/{post_id}")
+def update_post(post_id: int, updated_data: dict):
+    post_to_update = next((p for p in posts if p["id"] == post_id), None)
+    
+    for key, value in updated_data.items():
+        if key in post_to_update:
+            post_to_update[key] = value
+    
+    return {"message": "Post atualizado com sucesso", "post": post_to_update}
+
+@app.delete("/post/{post_id}")
+def delete_post(post_id: int):
+    post_index = next((i for i, p in enumerate(posts) if p["id"] == post_id), None)
+    print(post_index)
+    
+    deleted_post = posts.pop(post_index)
+    
+    return {
+        "message": "Post deletado com sucesso",
+        "deleted_post": deleted_post
+    }
